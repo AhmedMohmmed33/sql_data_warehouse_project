253ae99ee9fd1e496f1206727ab0242f87e2317e
Ahmed Mohamed Elsayed <ahmedelsayed6542404@gmail.com>
2025-02-28 22:12:57 +0200

    Update ddl_silver.sql

--- scripts /Silver/ddl_silver.sql
+++ scripts /Silver/ddl_silver.sql
@@ -19,12 +19,12 @@ IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
 GO
   
 CREATE TABLE silver.crm_prd_info(
-	prd_id			INT,
-	cat_id			VARCHAR(50),
-	prd_key			VARCHAR(50),
-	prd_nm			VARCHAR(50),
-	prd_cost		INT,
-	prd_line		VARCHAR(50),
+	prd_id		INT,
+	cat_id		VARCHAR(50),
+	prd_key		VARCHAR(50),
+	prd_nm		VARCHAR(50),
+	prd_cost	INT,
+	prd_line	VARCHAR(50),
 	prd_start_dt	DATE,
 	prd_end_dt  	DATE,
 	dwh_create_date	DATETIME2 DEFAULT GETDATE()
@@ -41,10 +41,10 @@ CREATE TABLE silver.crm_sales_details(
 	sls_cust_id  	INT,
 	sls_order_dt 	DATE,
 	sls_ship_dt  	DATE,
-	sls_due_dt		DATE,
+	sls_due_dt	DATE,
 	sls_sales    	INT,
 	sls_quantity	INT,
-	sls_price		INT,
+	sls_price	INT,
 	dwh_create_date	DATETIME2 DEFAULT GETDATE()
 );
 GO
@@ -54,9 +54,9 @@ IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
 GO
   
 CREATE TABLE silver.erp_cust_az12(
-	cid				VARCHAR(50),
-	bdate			DATE,
-	gen				VARCHAR(50),
+	cid		VARCHAR(50),
+	bdate		DATE,
+	gen		VARCHAR(50),
 	dwh_create_date	DATETIME2 DEFAULT GETDATE()
 );
 GO
@@ -66,8 +66,8 @@ IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
 GO
   
 CREATE TABLE silver.erp_loc_a101(
-	cid				VARCHAR(50),
-	cntry			VARCHAR(50),
+	cid		VARCHAR(50),
+	cntry		VARCHAR(50),
 	dwh_create_date	DATETIME2 DEFAULT GETDATE()
 );
 GO
@@ -77,8 +77,8 @@ IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
 GO
   
 CREATE TABLE silver.erp_px_cat_g1v2(
-	id				VARCHAR(50),
-	cat				VARCHAR(50),
+	id			VARCHAR(50),
+	cat			VARCHAR(50),
 	subcat			VARCHAR(50),
 	maintenance		VARCHAR(50),
 	dwh_create_date	DATETIME2 DEFAULT GETDATE()
